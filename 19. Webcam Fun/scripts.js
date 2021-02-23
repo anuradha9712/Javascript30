76904fe1e1c9bd39bab288aa16e519fd011551b1
Anuradha Aggarwal <anuradha.mcs18.du@gmail.com>
2021-02-24 01:34:25 +0530

    day 19

--- 19. Webcam Fun/scripts.js
+++ 19. Webcam Fun/scripts.js
@@ -35,7 +35,7 @@ function paintToCanvas() {
     // take the pixels out
     let pixels = ctx.getImageData(0, 0, width, height);
     // mess with them
-    // pixels = redEffect(pixels);
+    //pixels = redEffect(pixels);
 
     pixels = rgbSplit(pixels);
     // ctx.globalAlpha = 0.8;
@@ -55,15 +55,15 @@ function takePhoto() {
   const data = canvas.toDataURL('image/jpeg');
   const link = document.createElement('a');
   link.href = data;
-  link.setAttribute('download', 'handsome');
-  link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
+  link.setAttribute('download', 'Webcam Pic');
+  link.innerHTML = `<img src="${data}" alt="User Image" />`;
   strip.insertBefore(link, strip.firstChild);
 }
 
 function redEffect(pixels) {
   for (let i = 0; i < pixels.data.length; i += 4) {
-    pixels.data[i + 0] = pixels.data[i + 0] + 200; // RED
-    pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN
+    pixels.data[i + 0] = pixels.data[i + 0] + 100; // RED
+    pixels.data[i + 1] = pixels.data[i + 1] - 30; // GREEN
     pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // Blue
   }
   return pixels;
